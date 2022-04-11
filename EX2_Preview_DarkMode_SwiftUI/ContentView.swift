1065ce1bab1de72b6e068a73b253e291ddaff893
SongYunGeun <rjawoakstp@naver.com>
2022-04-11 18:02:23 +0900

    Initial Commit

---
+++ EX2_Preview_DarkMode_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EX2_Preview_DarkMode_SwiftUI
+//
+//  Created by 송윤근 on 2022/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EX2_Preview_DarkMode_SwiftUI/EX2_Preview_DarkMode_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EX2_Preview_DarkMode_SwiftUIApp.swift
+//  EX2_Preview_DarkMode_SwiftUI
+//
+//  Created by 송윤근 on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct EX2_Preview_DarkMode_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
