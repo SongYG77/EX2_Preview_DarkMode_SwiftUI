4311843b41473632707e7e8acb401cec93d36020
SongYunGeun <rjawoakstp@naver.com>
2022-04-15 15:53:27 +0900

    preview dark mode 연습

--- EX2_Preview_DarkMode_SwiftUI/ContentView.swift
+++ EX2_Preview_DarkMode_SwiftUI/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //다크모드 불러오기위해
+    //Enviroment 속성 래퍼 : 환경설정을 읽어오는 어노테이션
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text("Hello, SwiftUI")
+            .multilineTextAlignment(.center)
+            .padding(.all, 30.0)
+            .background( colorScheme == .light ? Color.white : Color.black )
+            .foregroundColor(Color.red)
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group{
+            //기본
+            ContentView()
+                .environment(\.colorScheme, .light)
+            
+            //다크모드
+            ContentView()
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
